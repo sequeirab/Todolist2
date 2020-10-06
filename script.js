acdb805e383a62d71b1ed76984ddac71b7d47949
Bruno Sequeira <sequeirab86@gmail.com>
2020-10-06 17:52:19 +0100

    better

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 
+
+
 function newList(){
 	let createList = document.createElement("LI");
 	let inputValue = document.getElementById("myInput").value;
 	let createNode = document.createTextNode(inputValue);	
 	createList.appendChild(createNode);
-	document.getElementById("myUL").appendChild(createList);
+	let appendX = document.createElement("SPAN")
+	let newEL = document.getElementById("myUL").appendChild(createList);
+	if(inputValue === ""){
+		alert("Insert a real To Do");
+	} else {
+		document.querySelector(newEl).appendChild(appendX);
+		appendX.className = "close";
+	}
 }
